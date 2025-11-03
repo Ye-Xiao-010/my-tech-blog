ec1b81c46b5b6593002cee72ffbc9214bdb0b4a9
YX <a15775774928@163.com>
2025-11-03 15:35:37 +0800

    Initial commit: Project skeleton with Spring Boot

---
+++ src/main/java/com/xiaofei/my_tech_blog/MyTechBlogApplication.java
@@ -0,0 +1,13 @@
+package com.xiaofei.my_tech_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTechBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTechBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xiaofei/my_tech_blog/MyTechBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaofei.my_tech_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTechBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
